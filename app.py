d47d0971d2f05d65d9103a76e526c444c34374cd
Armando Viana <armandovianafilho@hotmail.com>
2026-02-24 22:28:08 -0300

    deploy: Prepara projeto para Render (porta dinâmica)

--- app.py
+++ app.py
@@ -585,4 +585,6 @@ def set_security_headers(response):
     return response
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
